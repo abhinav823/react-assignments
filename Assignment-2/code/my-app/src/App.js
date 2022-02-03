97f3a5dbe84aef5f82d4ae973c181c72f327367b
abhinav823 <ap661746@gmail.com>
2022-02-03 21:20:32 +0530

    assign 2 submit

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-
-
-function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+const employee = {
+  name: 'Tom',
+  location: 'New York',
+  bloodGroup: 'B+',
+  age: 28,
+  profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
 }
 
 
+
+function App() {
+  return (
+    <div className='App'>
+      <img src={employee.profileImg} className="App-logo" alt={employee.name} />
+      <div className='user-abhi'>
+        <h2>{employee.name}</h2>
+
+        <p><strong>Location</strong>{employee.location}</p>
+        <p><strong>Bloodgroup</strong>{employee.bloodGroup}</p>
+        <p><strong>Age</strong>{employee.age}</p>
+
+      </div>
+    </div>
+
+  );
+
+
+
+}
 
 export default App;
